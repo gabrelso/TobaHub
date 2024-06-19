fc24e92c3a8e9ea1532b16d367c2a6da5a0814a3
r4pha.sln <105987360+r7pha@users.noreply.github.com>
2024-06-19 15:36:18 -0300

    Update TheStrongestBattlegrounds.lua

--- TheStrongestBattlegrounds.lua
+++ TheStrongestBattlegrounds.lua
@@ -1,2 +1,2 @@
 game.Players.LocalPlayer:Kick("Beta fechada. Entre em discord.gg/4bMVUPqPm2 para mais informações.")
-if setclipboard() then setclipboard("discord.gg/4bMVUPqPm2") end
+setclipboard("discord.gg/4bMVUPqPm2")
