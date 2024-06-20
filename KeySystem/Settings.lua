22fddcdf5a12d474deb21e04db91e6131f221122
r4pha.sln <105987360+r7pha@users.noreply.github.com>
2024-06-20 15:41:35 -0300

    Create Settings.lua

---
+++ KeySystem/Settings.lua
@@ -0,0 +1,12 @@
+local Settings = {
+	TitleText = "Toba<b>Hub</b>",
+	DescriptionText = "O script mais lgbt do mundo!",
+	UIGradientEnabled = false
+	ProgressBarEnabled = false,
+	AnimatedText = true,
+	GoofyBackground = true,
+	GoofyBackgroundID = 10527571475,
+	GetKeyLink = "https://hairyniggabutt.com.br/",
+}
+
+return Settings
